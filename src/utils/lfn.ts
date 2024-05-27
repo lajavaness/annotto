db0962df7d6ffb22f85bf65acba57d002151dc98
FelixJacobLJN <felix@lajavaness.com>
2024-05-27 13:51:52 +0200

    refactor: Remove utils/lfn.ts

--- src/utils/lfn.ts
+++
@@ -1,5 +0,0 @@
-export const booleanStringToBooleanOrUndefined = (str?: string): boolean | undefined => {
-  if (str === 'true' || str === 'True') return true
-  if (str === 'false' || str === 'False') return false
-  return undefined
-}
