f21bab359ae7d3693912276501349ab4f9ae2006
cheng <chenchengalex2014@gmail.com>
2024-06-25 11:13:30 +0200

    fix(front): Fix input space when textarea

--- annotto-front/src/modules/project/components/common/ZoneTools.js
+++ annotto-front/src/modules/project/components/common/ZoneTools.js
@@ -40,7 +40,7 @@ const ZoneTools = ({
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if ([27, 32].includes(event.keyCode)) {
+      if ([27, 32].includes(event.keyCode) && event.target.type !== 'textarea') {
         event.preventDefault()
         onSelectionChange()
       }
