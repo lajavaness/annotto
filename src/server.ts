e761b8c5ad1ee5652b34d2108fdf4f0128d918d3
Martin PICARD <martin@lajavaness.com>
2024-07-02 12:16:43 +0200

    fix: Set rate limit window to 1min

--- src/server.ts
+++ src/server.ts
@@ -55,7 +55,7 @@ const createServer = async (cfg: Config) => {
   const swaggerOptions = getSwaggerOptions(cfg)
   // Add rate limiter to prevent DoS attack:
   const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 60 * 1000, // 1 minute
     limit: 300, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
